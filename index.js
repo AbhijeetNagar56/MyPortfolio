e662edee40d91b3f52584cb4bc0d1c03051e2d5a
Abhii <nagarabhijeet0@gmail.com>
2025-07-07 17:37:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path');
 const port = 5000;
 const app = express();
+const iRoutes = require('./routes/internalRoutes');
+const uRoutes = require('./routes/userRoutes');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.send("<h1>Go to HomePage</h1>")
 });
+app.use('/', iRoutes);
+app.use('/user', uRoutes);
 
 
-app.listen(port, () => console.log(`Open http://localhost:${port}`));
-
+app.listen(port, () => console.log(`Open http://localhost:${port}/home`));
